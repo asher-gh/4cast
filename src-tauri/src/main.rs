affb345c58cf3f1649d73b10c1239ead49f62123
Asher <ashish.2017@hotmail.com>
2023-10-02 11:13:50 +0100

    Test shared state and command invokation from svelte.
    Testing a minimal setup to maintain a global state and invoke tauri commands from
    svelte.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(dead_code)]
+use std::sync::Mutex;
+
+#[derive(Default, Debug)]
+struct ChartData<'a> {
+    x: Vec<&'a str>,
+    y: Vec<&'a str>,
+}
+
+#[derive(Debug)]
+pub struct AppState<'a>(Mutex<ChartData<'a>>);
+
+#[tauri::command]
+fn log(state: tauri::State<AppState>) {
+    dbg!(state);
+}
+
+#[tauri::command]
+fn add_random(state: tauri::State<AppState>) {
+    let mut data = state.0.lock().unwrap();
+    data.x.push("hello");
+    data.y.push("world");
+}
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(AppState(Default::default()))
+        .invoke_handler(tauri::generate_handler![add_random, log])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script>
 	import Chart from '$lib/Chart.svelte';
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	async function add() {
+		await invoke('add_random');
+	}
+
+	async function log() {
+		await invoke('log');
+	}
 </script>
 
 <h1>ICU Bed Forecasts</h1>
+<button on:click={add}>add</button>
+<button on:click={log}>log</button>
 <Chart />
 
 <style>
